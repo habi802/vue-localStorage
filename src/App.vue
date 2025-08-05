7fbdde6069b077a88c6081e54f0ca2bb616edf7f
habi802 <shb8002@gmail.com>
2025-08-05 15:16:37 +0900

    기존 gallery 복사

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,48 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
+  import { watch, onMounted } from 'vue';
+  import { useRoute } from 'vue-router';
+  import { check } from './services/accountService';
+  import { useAccountStore } from './stores/account';
+
+  const route = useRoute();
+  const account = useAccountStore();
+
+  // 로그인 여부 확인
+  const checkAccount = async () => {
+    console.log('로그인 체크');
+    const res = await check();
+    console.log(res);
+    if (res === undefined || res.status !== 200) {
+      account.setChecked(false);
+      return;
+    }
+
+    account.setChecked(true);
+    account.setLoggedIn(res.data > 0);
+  }
+
+  onMounted(() => {
+    checkAccount();
+  })
+
+  watch(() => route.path, () => {
+    checkAccount();
+  });
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <template v-if="account.state.checked">
+    <Header />
+    <router-view></router-view>
+    <Footer />
+  </template>
+  <template v-else>
+    서버 통신 오류
+  </template>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,41 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/join',
+      name: 'join',
+      component: Join
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: Cart
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: Order
+    },
+    {
+      path: '/orders',
+      name: 'orders',
+      component: Orders
+    },
+    {
+      path: '/orders/:id',
+      name: 'orderDetail',
+      component: OrderDetail
+    },
   ],
 })
 
